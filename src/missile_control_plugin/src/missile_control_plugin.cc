75f9e0a939442a456b715adfc7c1cee4535afc07
AbiyyuMufti <akang.mufti@gmail.com>
2022-05-28 16:57:08 +0700

    preparing msgs for the control plugin

--- src/missile_control_plugin/src/missile_control_plugin.cc
+++ src/missile_control_plugin/src/missile_control_plugin.cc
@@ -1,6 +1,100 @@
 #include "missile_control_plugin.hh"
 
-namespace gazebo
+using namespace gazebo;
+
+GZ_REGISTER_MODEL_PLUGIN(MissileControl)
+
+MissileControl::MissileControl()
+{
+    this->booster_is_attached_ = false;
+    this->thrust_force_.x = 0.0;
+    this->thrust_force_.y = 0.0;
+    this->thrust_force_.z = 0.0;
+
+    this->thrust_torque_.x = 0.0;
+    this->thrust_torque_.y = 0.0;
+    this->thrust_torque_.z = 0.0;    
+}
+MissileControl::~MissileControl()
+{
+
+}
+
+void MissileControl::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+{
+    GZ_ASSERT(_model, "MissileControl _model pointer is NULL");
+    GZ_ASSERT(_sdf, "MissileControl _sdf pointer is NULL");
+
+    this->model_ = _model;
+
+    if (!this->FindLink("missile_body", _sdf, this->missile_body_link_))
+        return;
+    if (!this->FindJoint("booster_joint", _sdf, this->booster_joint_))
+        return;
+    if (!this->FindLink("booster", _sdf, this->booster_link_))
+        return;
+
+    // Listen to the update event. This event is broadcast every simulation iteration.
+    this->updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&MissileControl::OnUpdate, this));
+
+}
+
+bool MissileControl::FindJoint(const std::string &_sdfParam, sdf::ElementPtr _sdf, physics::JointPtr &_joint)
+{
+    // Check if the required element is found
+    if (!_sdf->HasElement(_sdfParam))
+    {
+        gzerr << "Unable to find the <" << _sdfParam << "> parameter." << std::endl;
+        return false;
+    }
+
+    // Load the joint name
+    std::string jointName = _sdf->Get<std::string>(_sdfParam);
+    _joint = this->model_->GetJoint(jointName);
+    if (!_joint)
+    {
+        gzerr << "Failed to find joint [" << jointName << "] aborting plugin load." << std::endl;
+        return false;
+    }
+    return true;    
+}
+
+bool MissileControl::FindLink(const std::string &_sdfParam, sdf::ElementPtr _sdf, physics::LinkPtr &_link)
+{
+    // Check if the required element is found
+    if (!_sdf->HasElement(_sdfParam))
+    {
+        gzerr << "Unable to find the <" << _sdfParam << "> parameter." << std::endl;
+        return false;
+    }
+
+    // Load the joint name
+    std::string linkName = _sdf->Get<std::string>(_sdfParam);
+    _link = this->model_->GetLink(linkName);
+    if (!_link)
+    {
+        gzerr << "Failed to find joint [" << linkName << "] aborting plugin load." << std::endl;
+        return false;
+    }
+    return true;    
+}
+
+void MissileControl::OnUpdate()
+{
+
+}
+
+void MissileControl::OnExocetCommand()
+{
+
+}
+
+void MissileControl::applyThrust()
+{
+
+}
+
+void MissileControl::detachingBooster()
 {
 
 }
--- src/missile_control_plugin/src/missile_control_plugin.hh
+++ src/missile_control_plugin/src/missile_control_plugin.hh
@@ -1,32 +1,93 @@
+#ifndef __MissileControl__
+#define __MissileControl__
+
 #include <functional>
-#include <ros/ros.h>
+// Gazebo
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
+#include <gazebo/common/Assert.hh>
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/physics/PhysicsTypes.hh>
+#include <gazebo/transport/transport.hh>
 #include <ignition/math/Vector3.hh>
+#include <ignition/common/Profiler.hh>
+#include <ignition/transport/Node.hh>
+
+// ROS
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/Wrench.h>
+#include <geometry_msgs/Vector3.h>
+#include <exocet_msgs/ExocetCMD.h>
+// #include <nav_msgs/Odometry.h>
+
+// Custom Callback Queue
+#include <ros/callback_queue.h>
+#include <ros/advertise_options.h>
+
+// Boost
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+
+#include <sdf/sdf.hh>
+#include <array>
+#include <mutex>
+
+
+
+
 
 namespace gazebo
 {
+    /// \brief Control the movement of the Exocet Missile by appying thrust to 
+    /// the missile's body or booster. It has the capability of detaching the booster
+    ///
+    /// The plugin requires the following parameters:
+    /// <missile_body>      Link name of the missile's main body
+    /// <booster>           Link name of the booster
+    /// <booster_joint>     Name of the joint controlling the left aileron.
+
     class MissileControl : public ModelPlugin
     {
-        public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
-        {
-        // Store the pointer to the model
-        this->model = _parent;
+    
+    public: 
+        MissileControl();
+        virtual ~MissileControl();
+        virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+        bool FindJoint(const std::string &_sdfParam, sdf::ElementPtr _sdf, physics::JointPtr &_joint);
+        bool FindLink(const std::string &_sdfParam, sdf::ElementPtr _sdf, physics::LinkPtr &_link);
+        void OnUpdate();
+        void OnExocetCommand();
+        void applyThrust();
+        void detachingBooster();
 
-        // Listen to the update event. This event is broadcast every
-        // simulation iteration.
-        this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-            std::bind(&ModelPush::OnUpdate, this));
+    private: 
+
+        /// \brief Pointer to the update event connection.
+        event::ConnectionPtr updateConnection;
+
+        /// \brief keep track of controller update sim-time.
+        private: gazebo::common::Time lastControllerUpdateTime;
+
+        /// \brief Controller update mutex.
+        boost::mutex mutex_;
+
+        physics::ModelPtr model_;
+        physics::LinkPtr booster_link_;
+        physics::LinkPtr missile_body_link_;
+        physics::JointPtr booster_joint_;
+
         
-        ROS_WARN("Loaded ModelPush Plugin with parent...%s", this->model->GetName().c_str());
-        }
 
-        // Called by the world update start event
-        public: void OnUpdate()
-        {
-        // Apply a small linear velocity to the model.
-        this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
-        }        
+        bool booster_is_attached_;
+        geometry_msgs::Vector3 thrust_force_;
+        geometry_msgs::Vector3 thrust_torque_;
+
     };
 }
+
+#endif
